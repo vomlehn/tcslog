0a7436dfd0f7845a4da261a5a24241986e711071
David VomLehn <dvomlehn@gmail.com>
2026-09-07 18:04:22 -0600

    Remove old code that was keep for reference.

--- tcslog.OLD/config.rs
+++
@@ -1,9 +0,0 @@
-/*
- * Configuration file for tcslog
- */
-
-/*
- * Timer resolution in seconds. Use when creating log file names to reduce
- * namespace collisions
- */
-const f64 TIMER_RESOLUTION = 1e-6;
